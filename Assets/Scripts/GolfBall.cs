3d9bc3a6630636ad129daec5b12656d55ad7f953
LibGDX Development <libgdx_development@DN0a1e6589.SUNet>
2020-10-31 16:03:04 -0700

    Added hit bar for golf ball, changed camera

--- Assets/Scripts/GolfBall.cs
+++ Assets/Scripts/GolfBall.cs
@@ -4,16 +4,74 @@ using UnityEngine;
 
 public class GolfBall : MonoBehaviour
 {
-    Vector3 velocity;
-    [SerializeField] private float acceleration = 10f;
+    // Physics Properties
+    [SerializeField] private float acceleration = 20f;
+    [SerializeField] private float deccelerationMagnitude = 0.1f;
 
+    [SerializeField] private float maxDistance = 20f;
+    [SerializeField] private float maxTime = 1f;
+
+    // Control Properties
+    [SerializeField] private float maxPixelDrag = 100f;
+
+    // Game Object Properties
+    [SerializeField] private GolfBallUI hitUI;
+
+    // Local variables
+    Vector3 velocity;
+    private Vector3 clickStart;
+    private bool moving = false;
+    private Vector3 proposedHit;
+
+    private void Awake()
+    {
+        velocity = new Vector3();
+    }
 
     void FixedUpdate()
     {
-        Vector2 playerInput = Vector2.ClampMagnitude(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")), 1f);
-        velocity.x += playerInput.x * Time.deltaTime * acceleration;
-        velocity.z += playerInput.y * Time.deltaTime * acceleration;
+        hitUI.IsAiming(moving);
+        if (velocity.magnitude < 0.001f)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                clickStart = Input.mousePosition;
+                moving = true;
+            }
+
+           if (Input.GetMouseButton(0))
+            {
+                proposedHit = (Input.mousePosition - clickStart);
+                if (proposedHit.magnitude > maxPixelDrag)
+                {
+                    proposedHit = proposedHit.normalized * maxPixelDrag;
+                }
+
+                float hitStrength = proposedHit.magnitude / maxPixelDrag;
+                Vector3 hitDirection = new Vector3(proposedHit.normalized.x, proposedHit.normalized.y, 0.0f);
+                hitUI.SetIndicator(hitDirection, hitStrength);
+            }
+
+            if (Input.GetMouseButtonUp(0) && moving)
+            {
+                moving = false;
+
+                velocity.x = - acceleration * proposedHit.x / maxPixelDrag;
+                velocity.z = - acceleration * proposedHit.y / maxPixelDrag;
+            }
+        }
+
         Vector3 displacement = velocity * Time.deltaTime;
         transform.localPosition += displacement;
+
+        Vector3 decceleration = -velocity.normalized * deccelerationMagnitude;
+
+        if (decceleration.magnitude > velocity.magnitude)
+        {
+            velocity = new Vector3();
+        } else
+        {
+            velocity += decceleration;
+        }
     }
 }
---
+++ Assets/Scripts/GolfBallUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GolfBallUI : MonoBehaviour
+{
+    [SerializeField] Image hitBar;
+    [SerializeField] Image powerBar;
+
+    public void IsAiming(bool isAiming)
+    {
+        hitBar.gameObject.SetActive(isAiming);
+    }
+
+    public void SetIndicator(Vector3 direction, float magnitude)
+    {
+        powerBar.transform.localScale = new Vector3(magnitude, 1f, 1f);
+        powerBar.color = new Color(Mathf.Clamp(2f * magnitude, 0f, 1f), Mathf.Clamp(2f - 2f * magnitude, 0f, 1f), 0f);
+
+        hitBar.transform.localPosition = -direction * 2.75f;
+        hitBar.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 180.0f + 180.0f * Mathf.Atan2(direction.y, direction.x) / Mathf.PI);
+    }
+}
