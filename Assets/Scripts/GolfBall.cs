86ca8a56c8c1e3c04cc0c403ce1dbf8e6ad35907
LibGDX Development <libgdx_development@DN0a1e6589.SUNet>
2020-10-31 00:01:20 -0700

    Added moving ball

---
+++ Assets/Scripts/GolfBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBall : MonoBehaviour
+{
+    Vector3 velocity;
+    [SerializeField] private float acceleration = 10f;
+
+
+    void FixedUpdate()
+    {
+        Vector2 playerInput = Vector2.ClampMagnitude(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")), 1f);
+        velocity.x += playerInput.x * Time.deltaTime * acceleration;
+        velocity.z += playerInput.y * Time.deltaTime * acceleration;
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
+    }
+}
