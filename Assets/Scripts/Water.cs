90b2a1c03b9c95845326f4c3da957e1dc86564eb
LibGDX Development <libgdx_development@DN0a1e6589.SUNet>
2020-11-08 00:16:03 -0800

    Added water

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : Ground
+{
+    override public Vector3 Friction(Vector3 currVelocity)
+    {
+        return new Vector3();
+    }
+}
