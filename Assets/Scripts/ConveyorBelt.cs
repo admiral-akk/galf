b6f905225e88848d4dad2f0aa183a9745e7f217f
LibGDX Development <libgdx_development@DN0a1e6589.SUNet>
2020-11-09 21:18:34 -0800

    Added more ice levels

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt: Ground
+{
+    [SerializeField] private Vector3 moveDirection = new Vector3(1, 0, 1);
+    [SerializeField] private float moveForce = 10f;
+
+    private void Awake()
+    {
+        moveDirection.y = 0;
+        moveDirection = moveDirection.normalized;
+    }
+
+    override public Vector3 Friction(Vector3 currVelocity)
+    {
+        return moveDirection * moveForce;
+    }
+}
--- Assets/Scripts/Ice.cs
+++ Assets/Scripts/Ice.cs
@@ -4,8 +4,4 @@ using UnityEngine;
 
 public class Ice : Ground
 {
-    override public Vector3 Friction(Vector3 currVelocity)
-    {
-        return new Vector3();
-    }
 }
